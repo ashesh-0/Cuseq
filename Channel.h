2556d2fbf8170ff1722a700a2ab500ec40b0390b
Ashesh <ashesh276@gmail.com>
2023-07-28 09:02:45 +0200

    channel.h not needed? commented out

--- Channel.h
+++ Channel.h
@@ -1,25 +1,25 @@
-#ifndef _Channel_H_
-#define _Channel_H_
+// #ifndef _Channel_H_
+// #define _Channel_H_
 
-#ifdef _MSC_VER
-// We use exception specifications to instruct SWIG to generate the correct
-// exception specifications for Java. Turn off the warnings that VC++ issues by
-// the mere use of exception specifications (which VC++ does not implement).
-#pragma warning(disable : 4290)
-#endif
-#include <string>
+// #ifdef _MSC_VER
+// // We use exception specifications to instruct SWIG to generate the correct
+// // exception specifications for Java. Turn off the warnings that VC++ issues by
+// // the mere use of exception specifications (which VC++ does not implement).
+// #pragma warning(disable : 4290)
+// #endif
+// #include <string>
 
-class Channel
-{
-public:
-private:
-    std::string config_;
-    std::string group_;
-    float exposure_;
-    bool doStack_;
-    float zOffset_;
-    unsigned int acquireEvery_;
-    std::string camera_;
-};
+// class Channel
+// {
+// public:
+// private:
+//     std::string config_;
+//     std::string group_;
+//     float exposure_;
+//     bool doStack_;
+//     float zOffset_;
+//     unsigned int acquireEvery_;
+//     std::string camera_;
+// };
 
-#endif _Channel_H_
+// #endif _Channel_H_
